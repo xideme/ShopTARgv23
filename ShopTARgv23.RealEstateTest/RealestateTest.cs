d0a742a2d4a41d7ee147a6da27d6e852b1612383
xideme <soopka@gmail.com>
2024-10-15 12:27:48 +0300

    xUnitTest

--- ShopTARgv23.RealEstateTest/RealestateTest.cs
+++ ShopTARgv23.RealEstateTest/RealestateTest.cs
@@ -27,5 +27,29 @@ namespace ShopTARgv23.RealEstateTest
             Assert.NotNull(result);
 
         }
+
+        [Fact]
+        public async Task ShouldNot_GetByIdRealestate_WhenReturnsNotEqual()
+        {
+            Guid wrongGuid = Guid.Parse(Guid.NewGuid().ToString());
+            Guid guid = Guid.Parse("58b815d8-284c-425a-8875-d74108e0cda5");
+
+            await Svc <IRealEstateServices>().GetAsync(guid);
+
+            Assert.NotEqual(wrongGuid, guid);
+        }
+
+        [Fact]
+
+        public async Task Should_GetByIdRealEstate_WhenReturnsEqual()
+        {
+            Guid correctGuid = Guid.Parse("58b815d8-284c-425a-8875-d74108e0cda5");
+            Guid guid = Guid.Parse("58b815d8-284c-425a-8875-d74108e0cda5");
+
+            await Svc<IRealEstateServices>().GetAsync(guid);
+
+            Assert.Equal(correctGuid, guid);
+
+        }
     }
 }
