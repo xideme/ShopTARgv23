ab0cf48b204f3ad78ac3116a8d0ad32226ddd925
xideme <soopka@gmail.com>
2024-12-03 12:27:27 +0200

    Login process and View

--- ShopTARgv23/Controllers/AccountsController.cs
+++ ShopTARgv23/Controllers/AccountsController.cs
@@ -69,5 +69,18 @@ namespace ShopTARgv23.Controllers
 
             return View();
         }
+
+        [HttpGet]
+        [AllowAnonymous]
+
+        public async Task <IActionResult> Login(string? returnUrl)
+        {
+            LoginViewModel vm = new()
+            {
+                ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList()
+            };
+
+            return View(vm);
+        }
     }
 }
